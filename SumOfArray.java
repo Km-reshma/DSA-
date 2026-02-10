5cc805cc635465006462ba26c683d7cdbdc4604e
Km-reshma <reshmak0811@gmail.com>
2026-02-10 22:09:31 +0530

    initial Commit

---
+++ SumOfArray.java
@@ -0,0 +1,16 @@
+public class SumOfArray{
+
+public static void main(String[] args){
+
+    int sum = 0;
+    
+    int[] arr = {2,5,9,7,5};
+
+    for(int i = 0; i < arr.length; i++){
+
+        sum= sum+arr[i];
+    }
+
+    System.out.println(sum);
+}
+}

a05e158276cf6ad3f75af8648a353b9fb6a4fd71
Km-reshma <RESHMAK0811@GMAIL.COM>
2026-03-26 12:06:39 +0530

    Time: 1 ms (22.74%) | Memory: 48.6 MB (9.13%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        Solve(0, s.length - 1, s);
+    }
+
+    public static void Solve(int i, int j, char[] s) {
+        if (i >= j) return;
+
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        Solve(i + 1, j - 1, s);
+    }
+}

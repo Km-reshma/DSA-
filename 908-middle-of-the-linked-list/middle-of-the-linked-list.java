f9102293142863d264ed7f5c8161412dae29ffe2
Km-reshma <RESHMAK0811@GMAIL.COM>
2026-04-12 10:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (60.39%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -8,16 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while (fast != null && fast.next != null) {
+//             slow = slow.next;        // 1 step
+//             fast = fast.next.next;   // 2 steps
+//         }
+
+//         return slow;
+//     }
+// }
+
+
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;        // 1 step
-            fast = fast.next.next;   // 2 steps
+        
+    //1. first we find the length of linked list
+        int count =0;
+        ListNode temp=head;
+        while(temp!=null){
+            count ++;
+            temp=temp.next;
         }
 
-        return slow;
+    //2. Find the mid
+         
+         int mid =count/2;
+
+         temp =head;
+
+    //3. Find the middle element
+        
+         for(int i=0; i<mid; i++){
+            temp=temp.next;
+
+         }
+         
+        return temp;
     }
 }
